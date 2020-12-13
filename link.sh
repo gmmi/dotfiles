cf3ea588d80593856315e59045b1d9feea2d9c98
gmmi <gmmi@gmmi.me>
2020-12-13 20:27:10 +0100

    basic checks

--- link.sh
+++ link.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+if ! [ -x "/usr/bin/curl"]; then
+  sudo apt install curl
+fi
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.zshrc ~/.zshrc
-ln -s $(pwd)/.config/i3/config ~/.config/i3/config
+if [ -d "~/.config/i3"]; then
+  ln -s $(pwd)/.config/i3/config ~/.config/i3/config
+fi
