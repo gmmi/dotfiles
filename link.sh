382f82cd7631af5772b13e7d42abe4de23f27cd8
gmmi <gmmi@gmmi.me>
2021-06-23 18:15:39 +0200

    Removed Linux-specifics

--- link.sh
+++ link.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
 
-if ! [ -x "/usr/bin/curl" ]; then
-  	sudo apt install curl
-fi
-
 if [ -e "~/.vim/autoload/plug.vim" ]; then
 	echo "[!] ~/.vim/autoload/plug.vim already exists" ]
 else
@@ -16,16 +12,8 @@ else
 	ln -s $(pwd)/.vimrc ~/.vimrc
 fi
 
-if ! [ -x "/usr/bin/zsh" ]; then
-	sudo apt install zsh
-fi
-
 if [ -e "~/.zshrc" ]; then
 	echo "[!] ~/.zshrc already exists"
 else
 	ln -s $(pwd)/.zshrc ~/.zshrc
-fi
-
-if [ -d "~/.config/i3" ]; then
-  ln -s $(pwd)/.config/i3/config ~/.config/i3/config
 fi
