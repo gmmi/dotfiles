03bc687c8c1df7487331e5a0e6744bad1b763431
gmmi <gmmi@gmmi.me>
2021-07-06 22:53:18 +0200

    I have no idea why this isn working as intended

--- link.sh
+++ link.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/bash
 
-if [ -e "~/.vim/autoload/plug.vim" ]; then
-	echo "[!] ~/.vim/autoload/plug.vim already exists" ]
+if [[ -x "/usr/bin/git" ]]; then
+	echo "[+] git installed"
+else
+	echo "[-] Installing git .."
+	sudo apt install git &> /dev/null
+fi
+
+if [[ -x "/usr/bin/curl" ]]; then
+	echo "[+] curl installed"
+else
+	echo "[-] Installing curl .."
+	sudo apt install curl &> /dev/null
+fi
+
+if [[ -e "~/.vim/autoload/plug.vim" ]]; then
+	echo "[!] ~/.vim/autoload/plug.vim already exists" 
 else
 	curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-if [ -e "~/.vimrc" ]; then
+if [[ -e "~/.vimrc" ]]; then
 	echo "[!] ~/.vimrc already exists"
 else
 	ln -s $(pwd)/.vimrc ~/.vimrc
 fi
 
-if [ -e "~/.zshrc" ]; then
+if [[ -e "~/.zshrc" ]]; then
 	echo "[!] ~/.zshrc already exists"
 else
 	ln -s $(pwd)/.zshrc ~/.zshrc
