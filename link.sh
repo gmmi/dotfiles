4dc3d5c427e3fd57d413d2ef8938713d88d5c4c6
gmmi <gmmi@gmmi.me>
2021-07-07 22:22:45 +0200

    I'm an idiot

--- link.sh
+++ link.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/bash
 
-if [[ -x "/usr/bin/git" ]]; then
+if [[ -x /usr/bin/git ]]; then
 	echo "[+] git installed"
 else
 	echo "[-] Installing git .."
 	sudo apt install git &> /dev/null
 fi
 
-if [[ -x "/usr/bin/curl" ]]; then
+if [[ -x /usr/bin/curl ]]; then
 	echo "[+] curl installed"
 else
 	echo "[-] Installing curl .."
 	sudo apt install curl &> /dev/null
 fi
 
-if [[ -e "~/.vim/autoload/plug.vim" ]]; then
+if [[ -e ~/.vim/autoload/plug.vim ]]; then
 	echo "[!] ~/.vim/autoload/plug.vim already exists" 
 else
 	curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-if [[ -e "~/.vimrc" ]]; then
-	echo "[!] ~/.vimrc already exists"
-else
+if [[ ! -e ~/.vimrc ]]; then
 	ln -s $(pwd)/.vimrc ~/.vimrc
+else
+	echo "[!] ~/.vimrc already exists"
 fi
 
-if [[ -e "~/.zshrc" ]]; then
-	echo "[!] ~/.zshrc already exists"
-else
+if [[ ! -e ~/.zshrc ]]; then
 	ln -s $(pwd)/.zshrc ~/.zshrc
+else
+	echo "[!] ~/.zshrc already exists"
 fi
